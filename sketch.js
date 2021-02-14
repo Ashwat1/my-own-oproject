7fd26bbb542d2a1ced2925ea0f2b9d555d65fde7
Ashwat1 <70321570+Ashwat1@users.noreply.github.com>
2021-02-14 22:07:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup(){
+  createCanvas(400,400);
+  var mario=createSprite(200,200,20,20);
+  var flyTrap =createSprite(30,200,10,10);
+  var bricks =createSprite(30,380,30,30);
+}
+
+function draw(){
+  drawSprites();
+}
